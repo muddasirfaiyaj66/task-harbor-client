11ddebcc6539e1f513a3d08f3e1be0ec8ed24220
Muddasir-Faiyaj <muddasirfaiyaj66@gmail.com>
2023-12-21 12:41:49 +0600

    setup react router, tailwind css and structured some folder

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,20 @@
+
+import {
+    createBrowserRouter
+} from "react-router-dom";
+import Main from "../Layouts/Main";
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Main></Main>,
+        children: [
+            {
+                index:true,
+                element:
+
+            }
+        ]
+    },
+]);
+
+export default router;
