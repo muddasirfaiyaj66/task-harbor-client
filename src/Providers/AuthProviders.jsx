c9387c5ae1451df69d1faa2c5bd651d362d21efc
Muddasir-Faiyaj <muddasirfaiyaj66@gmail.com>
2023-12-21 13:42:39 +0600

    auth provider setup complete

--- src/Providers/AuthProviders.jsx
+++ src/Providers/AuthProviders.jsx
@@ -1,10 +1,58 @@
-import { createContext } from "react";
-import {getAuth} from 'firebase/auth';
+import { createContext, useEffect, useState } from "react";
+import {GithubAuthProvider, GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile} from 'firebase/auth';
 import { app } from "../Config/Firebase/config.firebase";
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
 const AuthProviders = ({children}) => {
+    const [user , setUser]= useState(null);
+    const [ loading, setLoading]= useState(true);
+
+    const createUser = (email,password)=>{
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth,email,password);
+    }
+    const signIn = (email,password)=>{
+        setLoading(true);
+        return signInWithEmailAndPassword(auth,email,password);
+    }
+    const logOut = ()=>{
+        setLoading(true);
+        return signOut(auth);
+    }
+    const signInWithGoogle = ()=>{
+        setLoading(true);
+        return signInWithPopup(auth,googleProvider);
+    }
+    const signInWithGitHub = ()=>{
+        setLoading(true);
+        return signInWithPopup(auth, githubProvider);
+    }
+
+    const handleUpdateProfile = (name,photo)=>{
+        return updateProfile(auth.currentUser, {
+            displayName:name, photoURL:photo
+        })
+    }
+    useEffect(()=>{
+        const unsubscribe = onAuthStateChanged(auth,currentUser=>{
+            setUser(currentUser);
+            setLoading(false);
+        })
+        return ()=>{
+            return unsubscribe()
+        }
+    },[])
     const authInfo = {
+        createUser,
+        signIn,
+        logOut,
+        signInWithGoogle,
+        signInWithGitHub,
+        handleUpdateProfile
+
+
 
     }
     return (
